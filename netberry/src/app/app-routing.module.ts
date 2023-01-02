1826420690e268dfcea1d438f613608442ef6b40
Agustín <roel.ag@gmail.com>
2023-01-02 19:55:27 +0000

    [upd] background and canActivate

--- netberry/src/app/app-routing.module.ts
+++ netberry/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 
 const routes: Routes = [{ path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
- { path: 'tasks', loadChildren: () => import('./tasks/tasks.module').then(m => m.TasksModule) },
+ { path: 'tasks', loadChildren: () => import('./tasks/tasks.module').then(m => m.TasksModule), canActivate: [AuthGuard] },
  {
    path: '',
    redirectTo: 'tasks',
